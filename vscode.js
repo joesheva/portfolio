32d986285f3ea50e1159d27d550c0a7d61de249b
joesheva <159451033+joesheva@users.noreply.github.com>
2025-04-14 13:24:28 +0100

    Create vscode.js

---
+++ vscode.js
@@ -0,0 +1,24 @@
+
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+dropdownToggle.addEventListener('click', () => {
+  dropdownMenu.style.display = dropdownMenu.style.display === 'none' ? 'block' : 'none';
+});
+
+const dropdownToggle1 = document.querySelector('.dropdown-toggle1');
+const dropdownMenu1 = document.querySelector('.dropdown-menu1');
+
+dropdownToggle1.addEventListener('click', () => {
+  dropdownMenu1.style.display = dropdownMenu1.style.display === 'none' ? 'block' : 'none';
+});
+
+function openLanguage(languageName) {
+    var i;
+    var x = document.getElementsByClassName("language");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(languageName).style.display = "block";  
+  }
+
