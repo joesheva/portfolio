b4153247c17fac429535e970d849bb0ec3f35472
joesheva <159451033+joesheva@users.noreply.github.com>
2025-04-22 14:11:23 +0100

    Create youtube.js

---
+++ youtube.js
@@ -0,0 +1,513 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "24%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  const videoContainer = document.querySelector('.video-container');
+const video = document.querySelector('.video');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer.addEventListener('touchstart', function () {
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    });
+} else {
+    
+    videoContainer.addEventListener('mouseenter', function () {
+        video.play();
+    });
+
+    videoContainer.addEventListener('mouseleave', function () {
+        video.pause();
+        video.currentTime = 0;
+    });
+}
+
+const videoContainer1 = document.querySelector('.video-container1');
+const video1 = document.querySelector('.video1');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer1.addEventListener('touchstart', function () {
+        if (video1.paused) {
+            video1.play();
+        } else {
+            video1.pause();
+        }
+    });
+} else {
+   
+    videoContainer1.addEventListener('mouseenter', function () {
+        video1.play();
+    });
+
+    videoContainer1.addEventListener('mouseleave', function () {
+        video1.pause();
+        video1.currentTime = 0;
+    });
+}
+
+const videoContainer2 = document.querySelector('.shortvideo-container1');
+const video3 = document.querySelector('.video3');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer2.addEventListener('touchstart', function () {
+        if (video3.paused) {
+            video3.play();
+        } else {
+            video3.pause();
+        }
+    });
+} else {
+
+    videoContainer2.addEventListener('mouseenter', function () {
+        video3.play();
+    });
+
+    videoContainer2.addEventListener('mouseleave', function () {
+        video3.pause();
+        video3.currentTime = 0;
+    });
+}
+
+const videoContainer3 = document.querySelector('.shortvideo-container2');
+const video4 = document.querySelector('.video4');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer3.addEventListener('touchstart', function () {
+        if (video4.paused) {
+            video4.play();
+        } else {
+            video4.pause();
+        }
+    });
+} else {
+
+    videoContainer3.addEventListener('mouseenter', function () {
+        video4.play();
+    });
+
+    videoContainer3.addEventListener('mouseleave', function () {
+        video4.pause();
+        video4.currentTime = 0;
+    });
+}
+
+const videoContainer4 = document.querySelector('.shortvideo-container3');
+const video5 = document.querySelector('.video5');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer4.addEventListener('touchstart', function () {
+        if (video5.paused) {
+            video5.play();
+        } else {
+            video5.pause();
+        }
+    });
+} else {
+
+    videoContainer4.addEventListener('mouseenter', function () {
+        video5.play();
+    });
+
+    videoContainer4.addEventListener('mouseleave', function () {
+        video5.pause();
+        video5.currentTime = 0;
+    });
+}
+
+const videoContainer5 = document.querySelector('.video-container2');
+const video6 = document.querySelector('.video6');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer5.addEventListener('touchstart', function () {
+        if (video6.paused) {
+            video6.play();
+        } else {
+            video6.pause();
+        }
+    });
+} else {
+    
+    videoContainer5.addEventListener('mouseenter', function () {
+        video6.play();
+    });
+
+    videoContainer5.addEventListener('mouseleave', function () {
+        video6.pause();
+        video6.currentTime = 0;
+    });
+}
+
+const videoContainer6 = document.querySelector('.video-container3');
+const video7 = document.querySelector('.video7');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer6.addEventListener('touchstart', function () {
+        if (video7.paused) {
+            video7.play();
+        } else {
+            video7.pause();
+        }
+    });
+} else {
+    
+    videoContainer6.addEventListener('mouseenter', function () {
+        video7.play();
+    });
+
+    videoContainer6.addEventListener('mouseleave', function () {
+        video7.pause();
+        video7.currentTime = 0;
+    });
+}
+
+const videoContainer7 = document.querySelector('.video-container4');
+const video8 = document.querySelector('.video8');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer7.addEventListener('touchstart', function () {
+        if (video8.paused) {
+            video8.play();
+        } else {
+            video8.pause();
+        }
+    });
+} else {
+    
+    videoContainer7.addEventListener('mouseenter', function () {
+        video8.play();
+    });
+
+    videoContainer7.addEventListener('mouseleave', function () {
+        video8.pause();
+        video8.currentTime = 0;
+    });
+}
+
+const videoContainer8 = document.querySelector('.video-container5');
+const video9 = document.querySelector('.video9');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer8.addEventListener('touchstart', function () {
+        if (video9.paused) {
+            video9.play();
+        } else {
+            video9.pause();
+        }
+    });
+} else {
+    
+    videoContainer8.addEventListener('mouseenter', function () {
+        video9.play();
+    });
+
+    videoContainer8.addEventListener('mouseleave', function () {
+        video9.pause();
+        video9.currentTime = 0;
+    });
+}
+
+const videoContainer9 = document.querySelector('.news-container1');
+const video10 = document.querySelector('.video10');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer9.addEventListener('touchstart', function () {
+        if (video10.paused) {
+            video10.play();
+        } else {
+            video10.pause();
+        }
+    });
+} else {
+    
+    videoContainer9.addEventListener('mouseenter', function () {
+        video10.play();
+    });
+
+    videoContainer9.addEventListener('mouseleave', function () {
+        video10.pause();
+        video10.currentTime = 0;
+    });
+}
+
+const videoContainer10 = document.querySelector('.news-container2');
+const video11 = document.querySelector('.video11');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer10.addEventListener('touchstart', function () {
+        if (video11.paused) {
+            video11.play();
+        } else {
+            video11.pause();
+        }
+    });
+} else {
+    
+    videoContainer10.addEventListener('mouseenter', function () {
+        video11.play();
+    });
+
+    videoContainer10.addEventListener('mouseleave', function () {
+        video11.pause();
+        video11.currentTime = 0;
+    });
+}
+
+const videoContainer11 = document.querySelector('.news-container3');
+const video12 = document.querySelector('.video12');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer11.addEventListener('touchstart', function () {
+        if (video12.paused) {
+            video12.play();
+        } else {
+            video12.pause();
+        }
+    });
+} else {
+    
+    videoContainer11.addEventListener('mouseenter', function () {
+        video12.play();
+    });
+
+    videoContainer11.addEventListener('mouseleave', function () {
+        video12.pause();
+        video12.currentTime = 0;
+    });
+}
+
+const videoContainer12 = document.querySelector('.news-container4');
+const video13 = document.querySelector('.video13');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer12.addEventListener('touchstart', function () {
+        if (video13.paused) {
+            video13.play();
+        } else {
+            video13.pause();
+        }
+    });
+} else {
+    
+    videoContainer12.addEventListener('mouseenter', function () {
+        video13.play();
+    });
+
+    videoContainer12.addEventListener('mouseleave', function () {
+        video13.pause();
+        video13.currentTime = 0;
+    });
+}
+
+const videoContainer13 = document.querySelector('.news-container5');
+const video14 = document.querySelector('.video14');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer13.addEventListener('touchstart', function () {
+        if (video14.paused) {
+            video14.play();
+        } else {
+            video14.pause();
+        }
+    });
+} else {
+    
+    videoContainer13.addEventListener('mouseenter', function () {
+        video14.play();
+    });
+
+    videoContainer13.addEventListener('mouseleave', function () {
+        video14.pause();
+        video14.currentTime = 0;
+    });
+}
+
+const videoContainer14 = document.querySelector('.news-container6');
+const video15 = document.querySelector('.video15');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer14.addEventListener('touchstart', function () {
+        if (video15.paused) {
+            video15.play();
+        } else {
+            video15.pause();
+        }
+    });
+} else {
+    
+    videoContainer14.addEventListener('mouseenter', function () {
+        video15.play();
+    });
+
+    videoContainer14.addEventListener('mouseleave', function () {
+        video15.pause();
+        video15.currentTime = 0;
+    });
+}
+
+const videoContainer15 = document.querySelector('.news-container7');
+const video16 = document.querySelector('.video16');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer15.addEventListener('touchstart', function () {
+        if (video16.paused) {
+            video16.play();
+        } else {
+            video16.pause();
+        }
+    });
+} else {
+    
+    videoContainer15.addEventListener('mouseenter', function () {
+        video16.play();
+    });
+
+    videoContainer15.addEventListener('mouseleave', function () {
+        video16.pause();
+        video16.currentTime = 0;
+    });
+}
+
+const videoContainer16 = document.querySelector('.news-container8');
+const video17 = document.querySelector('.video17');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer16.addEventListener('touchstart', function () {
+        if (video17.paused) {
+            video17.play();
+        } else {
+            video17.pause();
+        }
+    });
+} else {
+    
+    videoContainer16.addEventListener('mouseenter', function () {
+        video17.play();
+    });
+
+    videoContainer16.addEventListener('mouseleave', function () {
+        video17.pause();
+        video17.currentTime = 0;
+    });
+}
+
+const videoContainer17 = document.querySelector('.news-container9');
+const video18 = document.querySelector('.video18');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer17.addEventListener('touchstart', function () {
+        if (video18.paused) {
+            video18.play();
+        } else {
+            video18.pause();
+        }
+    });
+} else {
+    
+    videoContainer17.addEventListener('mouseenter', function () {
+        video18.play();
+    });
+
+    videoContainer17.addEventListener('mouseleave', function () {
+        video18.pause();
+        video18.currentTime = 0;
+    });
+}
+
+const videoContainer18 = document.querySelector('.video-container6');
+const video19 = document.querySelector('.video19');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer18.addEventListener('touchstart', function () {
+        if (video19.paused) {
+            video19.play();
+        } else {
+            video19.pause();
+        }
+    });
+} else {
+    
+    videoContainer18.addEventListener('mouseenter', function () {
+        video19.play();
+    });
+
+    videoContainer18.addEventListener('mouseleave', function () {
+        video19.pause();
+        video19.currentTime = 0;
+    });
+}
+
+const videoContainer19 = document.querySelector('.video-container7');
+const video20 = document.querySelector('.video20');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer19.addEventListener('touchstart', function () {
+        if (video20.paused) {
+            video20.play();
+        } else {
+            video20.pause();
+        }
+    });
+} else {
+    
+    videoContainer19.addEventListener('mouseenter', function () {
+        video20.play();
+    });
+
+    videoContainer19.addEventListener('mouseleave', function () {
+        video20.pause();
+        video20.currentTime = 0;
+    });
+}
+
+const videoContainer20 = document.querySelector('.video-container8');
+const video21 = document.querySelector('.video21');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer20.addEventListener('touchstart', function () {
+        if (video21.paused) {
+            video21.play();
+        } else {
+            video21.pause();
+        }
+    });
+} else {
+    
+    videoContainer20.addEventListener('mouseenter', function () {
+        video21.play();
+    });
+
+    videoContainer20.addEventListener('mouseleave', function () {
+        video21.pause();
+        video21.currentTime = 0;
+    });
+}
+
+const videoContainer21 = document.querySelector('.video-container9');
+const video22 = document.querySelector('.video22');
+
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    videoContainer21.addEventListener('touchstart', function () {
+        if (video22.paused) {
+            video22.play();
+        } else {
+            video22.pause();
+        }
+    });
+} else {
+    
+    videoContainer21.addEventListener('mouseenter', function () {
+        video22.play();
+    });
+
+    videoContainer21.addEventListener('mouseleave', function () {
+        video22.pause();
+        video22.currentTime = 0;
+    });
+}
